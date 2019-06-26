d2e1d08ba9b2304aac53ab305dae7e1fe8b7f7ec
Christopher Cordine <crcordine@gmail.com>
2019-06-26 13:02:50 -0400

    modified package.json and server.js to ready application for build and deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 //Define Routes
 // Enter in the variable name that will hold the exported data from an external js file
@@ -27,6 +28,15 @@ mongoose
 app.use("/api/users", users);
 app.use("/api/items", items);
 app.use("/api/names", names);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+	// Set static folder
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+	});
+}
 
 const port = process.env.port || 5000;
 
